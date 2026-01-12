0c0c9dbb4da23dec6e26fb66e17d09ebc8401faf
ChaviduBandara <152202463+ChaviduBandara@users.noreply.github.com>
2026-01-12 20:19:32 +0530

    Created the patientQueue class

---
+++ src/main/java/HOSPITAL_PATIENT_MANAGEMENT_SYSTEM/PatientQueue.java
@@ -0,0 +1,40 @@
+package HOSPITAL_PATIENT_MANAGEMENT_SYSTEM;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class PatientQueue {
+    private final String speciality;
+    private final BlockingQueue<Patient> queue;
+
+    public PatientQueue(String speciality) {
+        this.speciality = speciality;
+        this.queue = new LinkedBlockingQueue<>();
+    }
+
+    // Adding a patient to the queue (called by the producer)
+    public void addPatient(Patient patient) throws InterruptedException{
+        int expectedSize = queue.size() + 1;
+        System.out.println("\t[QUEUE] Patient-" + patient.getPatientID() + " added to " + speciality + " queue. Queue size " + expectedSize);
+        queue.put(patient);
+
+    }
+
+    // Getting the next patient rom the queue (called by the consumer)
+    public Patient getPatient() throws InterruptedException {
+        return queue.take();  // Blocks if it is empty
+    }
+
+    // Checking if the queue is empty
+    public boolean isEmpty(){
+        return queue.isEmpty();
+    }
+
+    public int getSize(){
+        return queue.size();
+    }
+
+    public String getSpeciality(){
+        return speciality;
+    }
+}
